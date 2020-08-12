e609a4da1406e7adab0b69d531c4af38d3fa1c3a
unChae <darrun45@gmail.com>
2020-08-12 13:36:05 +0900

    .gitignore

--- deploy/modules/format.py
+++
@@ -1,9 +0,0 @@
-def price(n):
-    return n.replace(",","")
-
-def check_zero(n):
-    if n < 10:
-        n = "0" + str(n)
-    else:
-        n = str(n)
-    return n
--- python/getDate.py
+++
@@ -1,4 +0,0 @@
-from datetime import datetime
-import calendar
-print(datetime.today().month)
-print(calendar.monthrange(datetime.today().year, datetime.today().month)[1])
